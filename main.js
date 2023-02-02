f35ae37f38728e189edfb3b05ecdb19ca3a04125
Lenka Polcerova <lenka.polcerova@gmail.com>
2023-02-02 20:55:54 +0100

    splneny ukol

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // tady je místo pro náš program
 
+let vstup = document.querySelector('.vystupProgramu');
 
+vstup.innerHTML = prompt('Jméno:') + ' ' + prompt('Příjmení:') + ' ' + Number(2022 - prompt('Rok narození:'));
+vstup.style.color = prompt('Vaše oblíbená barva anglicky:');
 
 
